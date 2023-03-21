c756d182ec431526c1390e404f90067f6e145798
gustavo, ariel y sofia <prueba@gmail.com>
2023-03-21 12:02:52 -0300

    practica 1.2

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+ document.querySelector("body").innerHTML = `
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/GN7o-pznOlo" title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-
+        picture; web-share" allowfullscreen></iframe>
+    <p>Descripcion de musica jazz</p>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52523.52671422455!2d-58.83512015937408!3d-34.636504852210706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc945bc2149eff%3A0x72f476f24b92b3fa!2sMoreno%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1679403478529!5m2!1ses!2sar"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <p>como llegar a Moreno en menos de 1 horas</p>
+
+    <iframe style="border-radius:12px"
+        src="https://open.spotify.com/embed/album/6IZYetecA0WhfKhnqmI97a?utm_source=generator" width="100%" height="352"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <p>Musica jazz</p>
+
+        <iframe height="500px" width="1000px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+        <p>ubicacion de los productos</p>
+ `;
